dbc16484782fb936ddc63d8c874b8612a3c25943
David Cantuña <89049090+DavidCantunaMorales@users.noreply.github.com>
2022-10-30 17:57:37 -0500

    Update Principal.cpp

--- InterfazIngreso/Principal.cpp
+++ InterfazIngreso/Principal.cpp
@@ -4,15 +4,15 @@
 
 int main(int argc, char** argv) {
 	Validar obj1;
-    char dato[10];
-    strcpy(dato,obj1.ingresoInt("Ingrese un dato Entero: "));
-    std::cout<<std::endl;
-    std::cout<<dato<<std::endl;
-    strcpy(dato,obj1.ingresoDouble("Ingrese un dato Flotante o Double: "));
-    std::cout<<std::endl;
-    std::cout<<dato<<std::endl;
-    strcpy(dato,obj1.ingresoString("Ingrese un Texto: "));
-    std::cout<<std::endl;
-    std::cout<<dato<<std::endl;
+    	char dato[10];
+    	strcpy(dato,obj1.ingresoInt("Ingrese un dato Entero: "));
+    	std::cout<<std::endl;
+    	std::cout<<dato<<std::endl;
+    	strcpy(dato,obj1.ingresoDouble("Ingrese un dato Flotante o Double: "));
+    	std::cout<<std::endl;
+    	std::cout<<dato<<std::endl;
+    	strcpy(dato,obj1.ingresoString("Ingrese un Texto: "));
+    	std::cout<<std::endl;
+    	std::cout<<dato<<std::endl;
 	return 0;
 }
