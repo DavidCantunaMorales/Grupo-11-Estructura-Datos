cccb3c324e46fb228ce37b6f7d5b0ee5e7084ace
ArielGuevara <87491351+ArielGuevara@users.noreply.github.com>
2022-10-30 21:34:30 -0500

    Update Aplicacion.cpp

--- Ingreso de Datos abstracta/Aplicacion.cpp
+++ Ingreso de Datos abstracta/Aplicacion.cpp
@@ -12,13 +12,16 @@
 
 int main(int argc, char** argv){
 
-    ValidarDatos1 obj1;
-    char dato[10];
-    strcpy(dato,obj1.validarDoubles("Ingrese su dato"));
-    std::cout<<"\n"<<dato;
-    strcpy(dato,obj1.validarEntero("Ingrese su dato"));
-    std::cout<<"\n"<<dato;
-    strcpy(dato,obj1.validarLetras("Ingrese su texto"));
+    int valInt=0;
+    float valFloat=0;
+
+    ValidarDatos obj1;
+    std::string dato;
+    valInt = obj1.validarEntero("Ingrese su dato");
+    std::cout<<"\n"<<valInt;
+    valFloat = obj1.validarDoubles("Ingrese su dato");
+    std::cout<<"\n"<<valFloat;
+    dato = obj1.validarLetras("Ingrese su texto");
     std::cout<<"\n"<<dato;
     return 0;
 }
